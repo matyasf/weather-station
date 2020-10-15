0600595fade2cac2ad0db47218a677bd98627680
Matyas Forian Szabo <forianszm@gmail.com>
2020-10-15 14:14:56 +0200

    show if there is an error getting climacell data

--- controllers/ClimacellController.py
+++ controllers/ClimacellController.py
@@ -19,6 +19,7 @@ class ClimacellController:
     def __init__(self):
         self.future_forecasts: List[ClimacellResponse] = None
         self.font = ImageFont.truetype("assets/IBMPlexSans-Medium.ttf", 40)
+        self.error_msg = ""
 
     def fetch_weather(self):
         with concurrent.futures.ThreadPoolExecutor() as executor:
@@ -39,15 +40,22 @@ class ClimacellController:
         decoded: List[ClimacellResponse] = json.loads(response.text, object_hook=climacell_response_decoder)
         self.future_forecasts = [future_forecast for future_forecast in decoded if
                             future_forecast.observation_time > datetime.now(ZoneInfo(AppConstants.local_time_zone))]
+        self.error_msg = ""
         print("decoded")
 
     def display_data_if_any(self, display: AutoDisplay):
-        if self.future_forecasts is None:
-            return
-        image_draw = ImageDraw.Draw(display.frame_buf)
+        icon_y = 350
         text_y_start = 452
         column_width = 165
-        icon_y = 350
+        if self.error_msg:
+            image_draw = ImageDraw.Draw(display.frame_buf)
+            display.frame_buf.paste(0xFF, box=(5, icon_y, 780, icon_y + 245))
+            image_draw.multiline_text((5, icon_y), text=self.error_msg, font=self.font)
+            return
+        if self.future_forecasts is None:
+            return
+
+        image_draw = ImageDraw.Draw(display.frame_buf)
         display.frame_buf.paste(0xFF, box=(5, icon_y, 780, icon_y + 245))
         for num, forecast in enumerate(self.future_forecasts):
             # draw icon
@@ -61,7 +69,7 @@ class ClimacellController:
             icon_bmp = Image.open("assets/yr_icons_100/" + weather_icon + ".png")
             display.frame_buf.paste(icon_bmp, (10 + num * column_width, icon_y))
 
-            image_draw.text((10 + num * column_width, text_y_start ),
+            image_draw.text((10 + num * column_width, text_y_start),
                             text=forecast.observation_time.strftime("%H:%M"), font=self.font)
             image_draw.text((10 + num * column_width, text_y_start + 50),
                             text=str(forecast.temp) + "°C", font=self.font)
@@ -73,7 +81,9 @@ class ClimacellController:
 
     def on_future_complete(self, future: Future):
         if future.exception():
-            print("ClimacellController raised error: " + str(future.exception()))
+            self.error_msg = ":-[ " + repr(future.exception())
+            self.error_msg = '\n'.join(self.error_msg[i:i + 40] for i in range(0, len(self.error_msg), 40))
+            print("ClimacellController raised error: " + self.error_msg)
 
     def test_response(self):
         response = SimpleNamespace()
